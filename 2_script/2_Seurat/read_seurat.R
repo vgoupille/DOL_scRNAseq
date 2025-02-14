d68ed62c23e8978f343bcf1224dd602c9c287d27
vgoupille <goupille.valentin@live.fr>
2025-02-14 10:55:34 +0100

    Refactor read_seurat.R to improve readability and add gene and cell count output

--- 2_script/2_Seurat/read_seurat.R
+++ 2_script/2_Seurat/read_seurat.R
@@ -1,20 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# Notre repertoire de travail/ Faire un WORKINGDIR
-#WORKINGDIR="/home/genouest/cnrs_umr6553/vgoupille/DOL_scRNAseq/"
-#setwd(WORKINGDIR)
-#getwd()
- 
 
-#Important files which come from the STARsolo output are the ‘barcodes.tsv’, ‘features.tsv’ and ‘UniqueAndMult-Uniform.mtx’ files. These files are used to create the Seurat object. The ‘UniqueAndMult-Uniform.mtx’ file contains the gene expression data in matrix format. The ‘barcodes.tsv’ file contains the cell barcodes and the ‘features.tsv’ file contains the gene names.: 
-
-#Refer to three additional output files for further downstream analysis: the barcode.tsv, features.tsv and UniqueAndMult-Uniform.mtx. The first two files are generated in the ‘Solo.out/GeneFull/’ folder. The UniqueAndMult-Uniform.mtx is located in the ‘Solo.out/ GeneFull/Raw/’ folder.
-
-
+#singularity exec /home/genouest/cnrs_umr6553/vgoupille/DOL_scRNAseq/6_Containers/seurat.sif Rscript 2_script/2_Seurat/read_seurat.R
 
 # Charger les bibliothèques nécessaires
 library(Seurat)
-
+library (ggplot2)
 # Read Seurat object
 
 #4_results/1_script_results/2_resuts_seurat/DOL_scRNAseq_filtered.rds
@@ -29,21 +20,35 @@ DOL_scRNAseq_raw  <- readRDS("4_results/1_script_results/2_resuts_seurat/DOL_scR
 #head(DOL_scRNAseq_filtered, 100)
 head(DOL_scRNAseq_raw, 100) # je veux les 100 premières lignes
 
-Attaching SeuratObject
-                           orig.ident nCount_RNA nFeature_RNA
-AAACATCG_AAACATCG_AAACGATA   AAACATCG          3            3
-AACGTGAT_AAACATCG_AAACGATA   AACGTGAT          4            3
-AAACATCG_AACGTGAT_AAACGATA   AAACATCG          6            6
-AACGTGAT_AACGTGAT_AAACGATA   AACGTGAT          1            1
-AAACATCG_AAACATCG_ACTCGTAA   AAACATCG        150           21
-AACGTGAT_AAACATCG_ACTCGTAA   AACGTGAT        260           20
-AAACATCG_AACGTGAT_ACTCGTAA   AAACATCG        480           36
-AACGTGAT_AACGTGAT_ACTCGTAA   AACGTGAT        552           32
-AAACATCG_AAACATCG_CATGATCA   AAACATCG        763           60
-AACGTGAT_AAACATCG_CATGATCA   AACGTGAT       1353           66
-AAACATCG_AACGTGAT_CATGATCA   AAACATCG       1081           65
-AACGTGAT_AACGTGAT_CATGATCA   AACGTGAT       1023           54
-AAACATCG_AAACATCG_CTGCTTTG   AAACATCG       7535          348
-AACGTGAT_AAACATCG_CTGCTTTG   AACGTGAT       9007          349
-AAACATCG_AACGTGAT_CTGCTTTG   AAACATCG      23664          394
-AACGTGAT_AACGTGAT_CTGCTTTG   AACGTGAT      27104          398
+# Attaching SeuratObject
+#                            orig.ident nCount_RNA nFeature_RNA
+# AAACATCG_AAACATCG_AAACGATA   AAACATCG          3            3
+# AACGTGAT_AAACATCG_AAACGATA   AACGTGAT          4            3
+# AAACATCG_AACGTGAT_AAACGATA   AAACATCG          6            6
+# AACGTGAT_AACGTGAT_AAACGATA   AACGTGAT          1            1
+# AAACATCG_AAACATCG_ACTCGTAA   AAACATCG        150           21
+# AACGTGAT_AAACATCG_ACTCGTAA   AACGTGAT        260           20
+# AAACATCG_AACGTGAT_ACTCGTAA   AAACATCG        480           36
+# AACGTGAT_AACGTGAT_ACTCGTAA   AACGTGAT        552           32
+# AAACATCG_AAACATCG_CATGATCA   AAACATCG        763           60
+# AACGTGAT_AAACATCG_CATGATCA   AACGTGAT       1353           66
+# AAACATCG_AACGTGAT_CATGATCA   AAACATCG       1081           65
+# AACGTGAT_AACGTGAT_CATGATCA   AACGTGAT       1023           54
+# AAACATCG_AAACATCG_CTGCTTTG   AAACATCG       7535          348
+# AACGTGAT_AAACATCG_CTGCTTTG   AACGTGAT       9007          349
+# AAACATCG_AACGTGAT_CTGCTTTG   AAACATCG      23664          394
+# AACGTGAT_AACGTGAT_CTGCTTTG   AACGTGAT      27104          398
+
+# il y a 3 colonnes dans le tableau, orig.ident, nCount_RNA, nFeature_RNA
+# orig.ident est le nom de la cellule
+# nCount_RNA est le nombre de lecture pour cette cellule
+# nFeature_RNA est le nombre de gène pour cette cellule
+
+# dans cette experimentation seulement 16 "cellules" => regroupe plusieurs cellules à cause de l'histoire des barcodes
+
+
+# Print the number of cells and genes
+cat("Number of cells in the object: ", length(DOL_scRNAseq_raw), "\n")
+cat("Number of genes in the object: ", nrow(DOL_scRNAseq_raw), "\n")
+
+# Plot the number of genes per cell
